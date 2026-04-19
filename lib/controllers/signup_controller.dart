87edc7a20cd8073ae9b33b34a5d26ea546c93543
Abdallah Medhat <abdallahmedhat.dev@gmail.com>
2026-04-19 22:41:10 +0200

    Improving the signup_controller by adding The Email check pattern function and adding the Email validation function.

--- lib/controllers/signup_controller.dart
+++ lib/controllers/signup_controller.dart
@@ -18,6 +18,28 @@ class SignUpController extends GetxController {
   bool isPasswordVisible = false;
   bool isConfirmPassVisible = false;
 
+  /// Email Validation using RegExp
+  bool emailRegExp(String email) {
+    return RegExp(
+      r'^[a-z0-9]([a-z0-9_%+\-]|\.(?!\.))*[a-z0-9]@[a-z0-9][a-z0-9\-]*(\.[a-z0-9\-]+)*\.[a-z]{2,}$',
+    ).hasMatch(email);
+  }
+
+  String? isEmailValid(String email) {
+    if (email.isEmpty) {
+      return 'Please enter some text';
+    } else if (email.split("@").first.length > 64) {
+      return "Before <@> the string is too long";
+    } else if (email.length > 254) {
+      return "The Email is too long";
+    } else if (emailRegExp(email) == false) {
+      return "Please enter a valid email address";
+    } else {
+      return null;
+    }
+  }
+
+  /// Dealing with focus
   void setupfocus(FocusNode focusNode, Function(bool) onChange) {
     focusNode.addListener(() {
       onChange(focusNode.hasFocus);
@@ -25,11 +47,13 @@ class SignUpController extends GetxController {
     });
   }
 
+  /// Password Visibility function
   void passIconVisibility() {
     isPasswordVisible = !isPasswordVisible;
     update();
   }
 
+  /// Confirm Password Visibility
   void confirmPassIconVisibility() {
     isConfirmPassVisible = !isConfirmPassVisible;
     update();
