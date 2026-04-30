a57323fa5429578fdd5878b7dd74139d39508723
Abdallah Medhat <abdallahmedhat.dev@gmail.com>
2026-04-30 12:40:55 +0300

    Modifying the data file with the onupgrade function.

--- lib/repository/data.dart
+++ lib/repository/data.dart
@@ -1,8 +1,9 @@
+import 'dart:async';
 import 'package:sqflite/sqflite.dart';
 import 'package:path/path.dart';
 
 class SQLData {
-  Database? _db;
+  static Database? _db;
   //retrieve the database
   Future<Database?> get db async {
     if (_db == null) {
@@ -16,12 +17,17 @@ class SQLData {
   initializeDB() async {
     String path = await getDatabasesPath();
     String fullPath = join(path, "chatbot.db");
-    Database myDB = await openDatabase(fullPath, onCreate: _onCreate);
+    Database myDB = await openDatabase(
+      fullPath,
+      onCreate: _onCreate,
+      onUpgrade: _onUpgrade,
+      version: 1,
+    );
     return myDB;
   }
 
   //Create the database
-  _onCreate(Database db, int version) async {
+  FutureOr<void> _onCreate(Database db, int version) async {
     db.execute("""
   Create Table `users` (id INTEGER PRIMARY KEY AUTOINCREMENT,
   `EMAIL` TEXT NOT NULL,
@@ -32,4 +38,11 @@ class SQLData {
 """);
     print("Database Created Successfully =======================>>");
   }
+
+  //onUpgrade the database
+  FutureOr<void> _onUpgrade(
+    Database db,
+    int oldVersion,
+    int newVersion,
+  ) async {}
 }
